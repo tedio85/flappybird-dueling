f4457338313e2f06289043b8ae64a8289be91bf3
Ted Yu <tedio85@yahoo.com.tw>
2018-01-12 11:23:59 +0800

    working on creating train_op, not tested yet

--- dueling_network.py
+++ dueling_network.py
@@ -1,12 +1,23 @@
+import math
 import numpy as np
 import tensorflow as tf
 
-class DuelingNetwork(object):
-    def __init__(self, hps, training=True):
-        self.hps = hps
-        self.training = training
 
-        self._build_network()
+class DuelingNetwork(object):
+    def __init__(self, sess, hps, online=True, training=True):
+        self.sess = sess
+        self.hps = hps
+        self.online = online
+        self.training = training
+        self.global_step = tf.train.get_or_create_global_step()
+
+
+        scope_name = 'online' if online else 'target'
+        with tf.variable_scope(scope_name):
+            self._build_network()
+            if online:
+                self._build_train_op()
+
 
     def _build_network(self):
         # input
@@ -90,17 +101,61 @@ class DuelingNetwork(object):
 
         # action advantage stream, outputs 2 logits, one for each action
         advs = tf.layers.dense(inputs=flat, units=512, activation=tf.nn.relu, name='advantage_stream_in')
-        advantage = tf.layers.dense(inputs=advs, units=2, activation=None, name='advantage_stream_out')
+        advantage = tf.layers.dense(inputs=advs, units=self.hps.num_action, activation=None, name='advantage_stream_out') #(batch_size, 2)
 
         # merge the value stream and advantage stream
-        mean_adv = tf.reduce_mean(advantage, axis=1, keep_dims=True, name='mean_adv')
-        q_value = value + (advantage - mean_adv)
+        mean_adv = tf.reduce_mean(advantage, axis=1, keep_dims=True, name='mean_adv') #(batch_size, 1)
+        q_value = value + (advantage - mean_adv) #(batch_size, 2) - (batch_size, 1) using broadcast
         self.q_value = q_value
+        self.pred = tf.argmax(q_value, axis=1) # select action with highest Q value
 
-    #def _build_train_op(self):
+    def _build_train_op(self):
+
+        target_q = tf.placeholder(tf.float32, shape=[None, 1], name='y_i') # y_i
+
+        # TODO: need to compute Q(s,a|theta)
+
+        loss = tf.square(target_q - estimated_q)
+
+        # the mean and variance of batch norm layers has to be updated manually
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            optimizer = tf.train.AdamOptimizer(learning_rate=self.hps.lr)
+            grads_and_vars = optimizer.compute_gradients(loss)
+
+            ######## for testing ########
+            for i in grads_and_vars:
+                print('(', i[0],',',i[1],')')
+            #############################
+
+            # since network has 2 streams, scale the gradients by 1/sqrt(2) as stated in the paper
+            scale_factor = 1/math.sqrt(2)
+            scaled_grads_and_vars = [(val*scale_factor, name) for val, name in scaled_grads_and_vars]
+
+            # gradient clipping at 10 stated in paper
+            clip = self.hps.clip_value
+            clipped_grads_and_vars = [(tf.clip_by_value(val, -clip, clip), name) \
+                                      for val, name in scaled_grads_and_vars]
+
+            train_op = optimizer.apply_gradients(clipped_grads_and_vars, global_step=self.global_step)
 
 
     def _activation_summary(self, tensor):
         tensor_name = tensor.op.name
         tf.summary.histogram(tensor_name + '/activations', tensor)
         tf.summary.scalar(tensor_name + '/sparsity', tf.nn.zero_fraction(tensor))
+
+    def select_action(self, input_state):
+        # epsilon-greedy
+        if np.random.rand() < self.exploring_rate:
+            action = np.random.choice(num_action)  # Select a random action
+        else:
+            input_state = np.asarray(input_state).transpose([1, 2, 0]) #(4, 512, 288) => (512, 288, 4)
+            feed = {
+                self.input_state: input_state[None, :]
+            }
+            action = self.sess.run(self.pred, feed_dict=feed)
+
+        return action
+
+    #def train(self, s1, a, r, t, s2):
--- train.py
+++ train.py
@@ -1,13 +1,24 @@
+import os
+os.environ["SDL_VIDEODRIVER"] = "dummy" # make window not appear
+import tensorflow as tf
+import numpy as np
+import skimage.color
+import skimage.transform
+from ple.games.flappybird import FlappyBird
+from ple import PLE
+
 import tensorflow as tf
 import numpy as np
 
 from dueling_network import DuelingNetwork
 
-def get_default_hparams():
+def get_default_hparams(num_action=2):
     hparams = tf.contrib.training.HParams(
+                  num_action=num_action,
                   pic_width=288,
                   pic_height=512,
                   num_frames=4,
+                  clip_value=10,
                   dropout_rate=0.1,
                   lr=1e-4,
                   batch_size=64,
@@ -15,5 +26,12 @@ def get_default_hparams():
     return hparams
 
 if __name__ == '__main__':
-    hps = get_default_hparams()
-    dnet = DuelingNetwork(hps, training=True)
+    game = FlappyBird()
+    env = PLE(game, fps=30, display_screen=False) # environment interface to game
+    env.reset_game()
+    num_action = len(env.getActionSet())
+
+    hps = get_default_hparams(num_action)
+
+    with tf.Session() as sess:
+        dnet = DuelingNetwork(sess, hps, training=True)
